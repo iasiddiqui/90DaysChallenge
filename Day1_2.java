1b6cb9c40f357f29a2c9395c276932844d458656
Md Najeeb <63379987+najeebdev98@users.noreply.github.com>
2022-12-02 21:28:52 +0530

    Create Day1_2.java

---
+++ Day1_2.java
@@ -0,0 +1,16 @@
+//Addition of two numbers in JAVA
+
+class Challenge {
+  public static void main(String[] args) {
+    int a, b, sum;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter 1st No : ");
+    a = sc.nextInt();
+
+    System.out.println("Enter 2nd No : ");
+    b = sc.nextInt();
+
+    sum = a + b;
+    System.out.println("Addition of Two Numbers is = " + sum);
+  }
+} 
