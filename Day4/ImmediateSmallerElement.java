ce6242dcb64f19c3205a76c3ff297df3b5d3755a
Md Najeeb <63379987+najeebdev98@users.noreply.github.com>
2022-12-05 00:54:30 +0530

    Create ImmediateSmallerElement.java

---
+++ Day4/ImmediateSmallerElement.java
@@ -0,0 +1,14 @@
+//Immediate Smaller Element
+
+class Solution {
+    void immediateSmaller(int arr[], int n) {
+        // code here
+        for(int i=0; i<n-1; i++){
+            if(arr[i+1]>arr[i]){
+                arr[i+1]=arr[i];
+            }
+            else arr[i]=-1;;
+        }
+        arr[n-1]=-1;
+    }
+}
