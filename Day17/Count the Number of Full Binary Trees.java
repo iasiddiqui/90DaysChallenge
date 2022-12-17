d6f0120e35eebf9d01d594bc79a4004ba7160869
Md Najeeb <63379987+najeebdev98@users.noreply.github.com>
2022-12-18 00:42:28 +0530

    Create Count the Number of Full Binary Trees.java

---
+++ Day17/Count the Number of Full Binary Trees.java
@@ -0,0 +1,43 @@
+//Given an array arr[] of n integers, where each integer is greater than 1. The task is to find the number of Full binary tree from the given integers, such that each non-leaf node value is the product of its children value.
+
+
+class Solution {
+    
+    public static int numoffbt(int arr[], int n)
+    {
+        int maxValue = Integer.MIN_VALUE;
+        int minValue = Integer.MAX_VALUE;
+        int MOD = 1000000007;
+        for (int i = 0; i < n; i++)
+        {
+            maxValue = Math.max(maxValue, arr[i]);
+            minValue = Math.min(minValue, arr[i]);
+        }
+     
+        int mark[] = new int[maxValue + 1];
+        int value[] = new int[maxValue + 1];
+        Arrays.fill(mark, 0);
+        Arrays.fill(value, 0);
+        for(int i=0; i<n; ++i){
+            mark[arr[i]] = 1;// it is there
+            value[arr[i]] = 1; // single node forms a complete binary tree
+        }
+        int ans = 0;
+        for(int i=minValue; i<=maxValue; ++i){
+            if(mark[i] != 0){ // if it is prime, go for multiples and unmark them, similarly, if it is there in arr, find for other node to get a parent node which is in array
+                for(int j=i+i; j<=maxValue && j<=i*i; j+=i){
+                    if(mark[j] == 0){
+                        continue; // not there in array
+                    }
+                    value[j] = (value[j]+value[i]*value[j/i]%MOD)%MOD;
+                    // we can interchage left subtree and right subtree
+                    if(i != j/i){
+                        value[j] = (value[j]+value[i]*value[j/i]%MOD)%MOD;
+                    }
+                }
+            }
+            ans = (ans + value[i])%MOD;
+        }
+        return ans;
+    }
+}
