3238153e9df9d3b164c3a30d5d4cfa1666de2dbe
Md Najeeb <63379987+najeebdev98@users.noreply.github.com>
2022-12-19 00:20:31 +0530

    Create Java Inheritance.java

---
+++ Day18/Java Inheritance.java
@@ -0,0 +1,9 @@
+class cls2 extends cls1{
+    void mul(int p,int q){
+        System.out.println(p*q);
+    }
+    void task(int p,int q){
+        //Add your code here.
+        System.out.println(p*p+q*q);
+    }
+}
