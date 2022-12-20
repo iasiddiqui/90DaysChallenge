5fb4d86bb63216fbc67fc57d557682b17261faa8
Md Najeeb <63379987+najeebdev98@users.noreply.github.com>
2022-12-21 01:08:42 +0530

    Update and rename prg.java to Cyclically rotate an array by one.java

---
+++ Day20/Cyclically rotate an array by one.java
@@ -0,0 +1,18 @@
+
+//Given an array, rotate the array by one position in clock-wise direction.
+
+
+class Compute {
+    public void rotate(int arr[], int n){
+        int last = arr[arr.length-1];
+
+        for(int i=0;i<arr.length;i++){
+
+            int temp = arr[i];
+
+            arr[i] = last;
+
+            last = temp;
+        }
+    }
+}
