7dd0ad8550f0f04dc8f6f6d37af63656f901a525
Md Najeeb <63379987+najeebdev98@users.noreply.github.com>
2022-12-24 00:18:26 +0530

    Create Replace every element with the least greater element on its right.java

---
+++ Day23/Replace every element with the least greater element on its right.java
@@ -0,0 +1,23 @@
+//Given an array arr[] of N integers and replace every element with the least greater element on its right side in the array. If there are no greater elements on the right side, replace it with -1. 
+
+
+class Solution {
+    public static ArrayList<Integer> findLeastGreater(int n, int[] arr) {
+        // code here
+        TreeSet<Integer> ts=new TreeSet<>();
+        int a[]=new int[n];
+        for(int i=n-1;i>=0;i--){
+            if(ts.ceiling(arr[i]+1)==null){
+                a[i]=-1;
+            }else{
+                a[i]=ts.ceiling(arr[i]+1);
+            }
+            ts.add(arr[i]);
+        }
+        ArrayList<Integer> A=new ArrayList<>();
+        for(int i:a){
+            A.add(i);
+        }
+        return A;
+    }
+}  
