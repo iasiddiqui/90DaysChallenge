baa9fc7b4fdbbf43c84c7e58a95f0cb0e205b9f9
Md Najeeb <63379987+najeebdev98@users.noreply.github.com>
2022-12-31 20:58:18 +0530

    Update and rename p.java to Series AP.java

---
+++ Day30/Series AP.java
@@ -0,0 +1,19 @@
+//Given the first 2 terms A1 and A2 of an Arithmetic Series.Find the Nth term of the series. 
+
+
+ 
+class Solution {
+
+    int nthTermOfAP(int A1, int A2, int N) {
+
+       int d,a=0;
+
+       d=A2-A1;
+
+       a=A1+(N-1)*d;
+
+       return a;
+
+    }
+
+}
