3899f34231b8fea30a44c7b9018d45a5f3571b99
Md Najeeb <63379987+najeebdev98@users.noreply.github.com>
2023-01-07 00:09:16 +0530

    Create GCD of two numbers.java

---
+++ Day37/GCD of two numbers.java
@@ -0,0 +1,29 @@
+//Given two positive integers A and B, find GCD of A and B.
+
+class Solution
+{
+    public int gcd(int A , int B) 
+    { 
+        if(A == 0){
+            return B;
+        }
+        
+        if(B == 0){
+            return A;
+        }
+        
+        if(A == B){
+            return A;
+        }
+        
+        if(A > B){
+            return gcd(B, A % B);
+        }
+        
+        if(B > A){
+            return gcd(B % A, A);
+        }
+        
+        return -1;
+    } 
+}
