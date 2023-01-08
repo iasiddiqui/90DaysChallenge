d884c1df70972d57a649b1301c74a55268585ae3
Md Najeeb <63379987+najeebdev98@users.noreply.github.com>
2023-01-09 02:19:45 +0530

    Update and rename prog.java to Remainder Evaluation.java

---
+++ Day39/Remainder Evaluation.java
@@ -0,0 +1,8 @@
+//Given two positive integers num1 and num2, the task is to find the remainder when num1 is divided by num2.
+
+class Solution{
+    static int findRemainder(int num1,int num2){
+        // code here
+        return num1%num2;
+    }
+}
