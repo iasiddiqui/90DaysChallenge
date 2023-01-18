fbbcde8dfbab0cbc75e6ec0bf50133eb72b04305
Md Najeeb <63379987+najeebdev98@users.noreply.github.com>
2023-01-18 08:32:47 +0530

    Create Sort Array.java

---
+++ Day48/Sort Array.java
@@ -0,0 +1,13 @@
+//Given a random set of numbers, Print them in sorted order.
+
+
+
+class Solution 
+{ 
+    int[] sortArr(int[] arr, int n) 
+    { 
+        // code here
+        Arrays.sort(arr);
+        return arr;
+    }
+} 
