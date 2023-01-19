93bb96c05467de6098a81b680a3195779914165b
Md Najeeb <63379987+najeebdev98@users.noreply.github.com>
2023-01-20 00:05:55 +0530

    Update Union of two arrays.java

--- Day50/Union of two arrays.java
+++ Day50/Union of two arrays.java
@@ -1,7 +1,5 @@
 //Given two arrays a[] and b[] of size n and m respectively. The task is to find the number of elements in the union between these two arrays.
-
 //Union of the two arrays can be defined as the set containing distinct elements from both the arrays. If there are repetitions, then only one occurrence of element should be printed in the union.
-
 //Note : Elements are not necessarily distinct.
 
 class Solution{
