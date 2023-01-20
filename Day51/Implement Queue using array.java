50a16f7b805fc927358bcc7fa88e61e542b5b1d2
Md Najeeb <63379987+najeebdev98@users.noreply.github.com>
2023-01-21 00:23:23 +0530

    Update Implement Queue using array.java

--- Day51/Implement Queue using array.java
+++ Day51/Implement Queue using array.java
@@ -3,30 +3,29 @@
 //(ii) 2     (a query of this type means to pop element from queue and print the poped element)
 
 class MyQueue {
-
     int front, rear;
-    int arr[] = new int[100005];
+	int arr[] = new int[100005];
 
     MyQueue()
-    {
-        front=0;
-        rear=0;
-    }
-    
-    //Function to push an element x in a queue.
-    void push(int x)
-    {
-        arr[rear++] = x;
-    } 
+	{
+		front=0;
+		rear=0;
+	}
+
+	//Function to push an element x in a queue.
+	void push(int x){
+     arr[rear++]=x;
+     return;
+    }  
 
     //Function to pop an element from queue and return that element.
-    int pop()
+	int pop()
     {
-        if(rear == front){
-            front = 0;
-            rear = 0;
+     if(rear==front) {
             return -1;
         }
-        return arr[front++];
+        int aa=arr[front];
+        front++;
+        return aa;
     } 
 }
