ab01eb2b8750d30da99dda8b20a89f7d44b9a938
Md Najeeb <63379987+najeebdev98@users.noreply.github.com>
2023-01-24 01:03:48 +0530

    Create Binary Array Sorting.java

---
+++ Day54/Binary Array Sorting.java
@@ -0,0 +1,16 @@
+//Given a binary array A[] of size N. The task is to arrange the array in increasing order.
+//Note: The binary array contains only 0  and 1.
+ 
+class Solution
+{
+    //Function to sort the binary array.
+    static void binSort(int A[], int N)
+    {
+        // add your code here
+        Arrays.sort(A);
+        
+        /**************
+        * No need to print the array
+        * ************/
+    }
+}
