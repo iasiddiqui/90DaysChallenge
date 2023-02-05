df54ec4e37d7fb21ec131b0caa8201e091959c08
Md Najeeb <63379987+najeebdev98@users.noreply.github.com>
2023-02-06 00:31:39 +0530

    Create Pattern 11.java

---
+++ Day67/Pattern 11.java
@@ -0,0 +1,12 @@
+//Geek is very fond of patterns. Once, his teacher gave him a pattern to solve. He gave Geek an integer n and asked him to build a pattern.
+
+class Solution {
+    void printTriangle(int n) {
+        for(int i=1; i<=n;i++){
+            for(int j=1; j<=i;j++){
+                System.out.print(((j+i)%2==0?1:0)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
