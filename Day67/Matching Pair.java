637fa40b46df86b563f516e22292cea507993215
Md Najeeb <63379987+najeebdev98@users.noreply.github.com>
2023-02-06 00:17:40 +0530

    Update and rename prog.java to Matching Pair.java

---
+++ Day67/Matching Pair.java
@@ -0,0 +1,8 @@
+//Given a set of numbers from 1 to N, each number is exactly present twice so there are N pairs. In the worst-case scenario, how many numbers X should be picked and removed from the set until we find a matching pair?
+
+class Solution{
+    static long find(int N){
+        // code here
+        return (N+1);
+    }
+}
